8fc6fe8c955b2ff42a0854c9bfbd228295785004
James Rchrdsn <laurent.james63@gmail.com>
2024-04-08 12:05:13 +0200

    renvoie vers proprerty

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import logementData from "../../assets/data/logements.json";
+import { Link } from "react-router-dom";
 
-function Card({ title, cover }) {
+function Card({ id, title, cover }) {
   return (
-    <div className="card">
-      <img src={cover} alt={title} />
-      <h2>{title}</h2>
-    </div>
+    <Link to={`/property/${id}`}>
+      <div className="card">
+        <img src={cover} alt={title} />
+        <h2>{title}</h2>
+      </div>
+    </Link>
   );
 }
 
@@ -20,57 +23,15 @@ function CardList() {
   return (
     <div className="card-list">
       {propertyData.map((property, index) => (
-        <Card key={index} title={property.title} cover={property.cover} />
+        <Card
+          key={index}
+          id={property.id}
+          title={property.title}
+          cover={property.cover}
+        />
       ))}
     </div>
   );
 }
 
 export default CardList;
-
-// import React, { useState, useEffect } from "react";
-
-// function Card({ title, cover }) {
-//   return (
-//     <div className="card">
-//       <img src={cover} alt={title} />
-//       <h2>{title}</h2>
-//     </div>
-//   );
-// }
-
-// function CardList() {
-//   const [propertyData, setPropertyData] = useState([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch(
-//           "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json"
-//         );
-//         if (!response.ok) {
-//           throw new Error("Erreur lors du chargement des données: 1");
-//         }
-//         const data = await response.json();
-//         setPropertyData(data);
-//       } catch (error) {
-//         console.error(
-//           "Erreur lors du chargement des données: 2",
-//           error.message
-//         );
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="card-list">
-//       {propertyData.map((property, index) => (
-//         <Card key={index} title={property.title} cover={property.cover} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CardList;
